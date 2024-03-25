e479f67fe774866489fdc4459ec5dcb13221dacf
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2024-03-25 18:51:55 +0000

    commit

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_.._Dip047X0.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/__D4BMSgal.mjs');
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_CLAVrWvv.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_Bo1gBtrp.mjs');
+
+export { page };
